c49f090915e1cbca8b5011a90ab5086fac2e8af7
nadia3190 <nadia.tlb.90@gmail.com>
2023-08-12 14:31:08 +0200

    adding app.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,13 @@
-const express = require('express');// Import express module pour créer le serveur 
+const http = require('http'); // Import du module http
+const app = require('./app'); // Import du fichier app.js
+const PORT = process.env.PORT || 3000; // Port du serveur
 
-const app = express();// Créer le serveur 
-const PORT = process.env.PORT || 3000;// Port du serveur  
+app.set('port', PORT); // Définition du port utilisé par l'application
+const server = http.createServer(app); // Créer le serveur en utilisant app
 
-app.get('/', (req, res) => {// Route pour la page d'accueil
-  res.send('Response from server.js');// Réponse du serveur 
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
-app.listen(PORT, () => {// Ecoute du serveur 
-  console.log(`Server is running on port ${PORT}`);// Message de confirmation 
-});
+
 //server.js permet de créer un serveur et de l'écouter sur le port 3000. 
-//express est un module qui permet de créer un serveur.
